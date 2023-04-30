b6eb4462e90cff151dd6423d01401e4a764d473e
Crowdbotics <team@crowdbotics.com>
2023-04-30 16:02:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "matlacha-civic-40363",
+  url: "https://matlacha-civic-40363.botics.co",
+  api: "https://matlacha-civic-40363.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
